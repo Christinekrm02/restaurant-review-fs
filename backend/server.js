ba69bee67f8738c6242abf9b2c97aae8dac05e6a
Christine Maynard <christinekrm02@gmail.com>
2021-05-09 10:10:34 -0400

    First commit initialize package.json and install dependencies add type=modules to package.json for es6 syntax use create server.js and initialize app

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import restaurants from "./api/restaurants.route.js";
+
+const app = express();
+//apply cors middleware to app to allow for browser to browser requests
+app.use(cors());
+//express.json() remove the need to use bodyParser
+//server will be able to accept JSON in the body of the request
+app.use(express.json());
+
+app.use("/api/v1/restaurants", restaurants);
+//404 error route
+app.use("*", (req, res) => {
+  res.status(404).json({ error: "Page not found" });
+});
+
+export default app;
